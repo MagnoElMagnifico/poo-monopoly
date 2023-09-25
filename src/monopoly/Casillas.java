0c68129346bab416c232b34ddd80d61fe9189c01
Gabriel <gabrielop2004@gmail.com>
2023-09-25 14:00:24 +0200

    Cambies en casilla

--- src/monopoly/Casillas.java
+++ src/monopoly/Casillas.java
@@ -1,4 +1,9 @@
 package monopoly;
 
 public class Casillas {
+    private String nombre, Propietario;
+    private String Tipo;
+    private int Precio, Hipoteca;
+    private int Valor_casa, valor_hotel
+
 }
