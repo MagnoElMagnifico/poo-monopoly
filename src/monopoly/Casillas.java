654cdfe43c4478d0faa932696734028fda5005b5
Gabriel <gabrielop2004@gmail.com>
2023-09-25 18:23:54 +0200

    Actualizacion variables casillas

--- src/monopoly/Casillas.java
+++ src/monopoly/Casillas.java
@@ -4,6 +4,6 @@ public class Casillas {
     private String nombre, Propietario;
     private String Tipo;
     private int Precio, Hipoteca;
-    private int Valor_casa, valor_hotel
+    private int Valor_casa, valor_hotel;
 
 }
