fb097c54d9dd3f4cc85680681e1a309c23392670
MarcosG <marcosgranja317@gmail.com>
2023-09-29 23:44:32 +0200

    Añadir comandos con argumentos + Refactorización de Monopoly

--- src/monopoly/Monopoly.java
+++ src/monopoly/Monopoly.java
@@ -14,7 +14,7 @@ import monopoly.utilidades.Formatear.Estilo;
  * @date 25-09-2023
  */
 public class Monopoly {
-    private final String msgAyuda;
+    private String msgAyuda;
     private Scanner scanner;
     private Dado dado;
 
@@ -23,8 +23,9 @@ public class Monopoly {
         dado = new Dado();
 
         msgAyuda = """
-               \tayuda, help\t\tMuestra esta información de ayuda.
-               """;
+                        ayuda, help      Muestra esta información de ayuda.
+                        salir, quit      Cierra el programa.
+                """;
     }
 
     /**
@@ -32,30 +33,73 @@ public class Monopoly {
      * Muestra el Prompt ("$>") y permite al usuario escribir un comando.
      */
     public void iniciarConsola() {
-        while(true) {
+        while (true) {
             System.out.print(Formatear.con("$> ", Color.Verde, Estilo.Negrita));
-            this.procesarComando(scanner.nextLine());
+            this.procesarCmd(scanner.nextLine());
         }
     }
 
-    /** Procesa el comando dado y realiza las llamadas pertinentes para ejecutarlo  */
-    private void procesarComando(String cmd) {
+    /** Procesa el comando dado y realiza las llamadas pertinentes para ejecutarlo */
+    private void procesarCmd(String cmd) {
         // Ignorar comandos en blanco o con solo espacios
         if (cmd.isBlank()) {
             return;
         }
 
-        // Normalizar
-        String cmdNorm = cmd.strip().replaceAll("\\s+", " ").toLowerCase();
-
-        // Separar en argumentos
-        // String[] args = cmdNorm.split("\\s");
+        // Normalizar:
+        //   - Eliminar espacio al inicio y al final
+        //   - Eliminar más de dos espacios seguidos
+        //   - Convertir a minúsculas
+        String cmdNorm = cmd.strip().replaceAll(" +", " ").toLowerCase();
 
         // Ver: https://docs.oracle.com/en/java/javase/17/language/switch-expressions.html
         System.out.print(switch (cmdNorm) {
-            case "ayuda", "help"          -> this.msgAyuda;
-            case "lanzar", "lanzar dados" -> "Resultado: %s\n".formatted(Formatear.con(Integer.toString(dado.lanzar2Dados()), Color.Azul));
-            default                       -> Formatear.con("El comando \"%s\" no está soportado\n".formatted(cmdNorm), Color.Rojo);
+            case "ayuda", "help" -> this.msgAyuda;
+            case "salir", "quit" -> {
+                System.exit(0);
+                yield ""; // Si no devuelvo un objeto da error
+            }
+            case "lanzar", "lanzar dados" ->
+                    "Resultado: %s\n".formatted(Formatear.con(Integer.toString(dado.lanzar2Dados()), Color.Azul));
+            // TODO:
+            // case "jugador"
+            // case "acabar turno"
+            // case "salir carcel"
+            // case "ver tablero"
+            // case "listar jugadores"
+            // case "listar avatares"
+            // case "listar enventa"
+            default -> this.cmdConArgumentos(cmdNorm);
         });
+    }
+
+    /**
+     * Función de ayuda que procesa y ejecuta un comando con argumentos.
+     *
+     * @param cmd Comando previamente normalizado.
+     * @return String con el resultado del comando.
+     */
+    private String cmdConArgumentos(String cmd) {
+        String[] args = cmd.split(" ");
+        return switch (args[0]) {
+            // TODO:
+            // case "crear"
+            // case "comprar"
+            // case "describir"
+
+            // A modo de prueba:
+            case "format", "formatear" -> {
+                if (args.length != 2) {
+                    yield Formatear.con("Número de argumentos incorrecto: recibí %d, esperaba 2\n".formatted(args.length), Color.Rojo);
+                }
+
+                try {
+                    yield Formatear.num(Long.parseLong(args[1])) + "\n";
+                } catch (NumberFormatException e) {
+                    yield Formatear.con("\"%s\" no es un número válido\n".formatted(args[1]), Color.Rojo);
+                }
+            }
+            default -> Formatear.con("\"%s\": Comando no válido\n".formatted(args[0]), Color.Rojo);
+        };
     }
 }
