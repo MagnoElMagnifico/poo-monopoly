22edce10824c2691e75806820c7877072d18e66d
MarcosG <marcosgranja317@gmail.com>
2023-10-02 12:48:21 +0200

    Creación de la clase Avatar a partir de la del Jugador

---
+++ src/monopoly/Avatar.java
@@ -0,0 +1,54 @@
+package monopoly;
+
+public class Avatar {
+    /** Tipos de avatares posibles */
+    public enum TipoAvatar {
+        Coche, Esfinge, Sombrero, Pelota
+    }
+
+    private TipoAvatar tipo;
+    private char id;
+    private String casilla;
+    private Jugador jugador;
+
+    /** Crea un avatar dado su tipo, id y el jugador al que hace referencia */
+    public Avatar(TipoAvatar tipo, char id, Jugador jugador) {
+        this.tipo = tipo;
+        this.id = id;
+        this.casilla = "inicial";
+        this.jugador = jugador;
+    }
+
+    /** Mueve el avatar un determinado número de casillas */
+    public void mover(int nCasillas) {
+        casilla = "no inicial";
+    }
+
+    @Override
+    public String toString() {
+        return """
+                {
+                    id: %c
+                    tipo: %s,
+                    casilla: %s
+                    jugador: %s
+                }
+                """.formatted(id, tipo, casilla, jugador.getNombre());
+    }
+
+    public char getId() {
+        return id;
+    }
+
+    public TipoAvatar getTipo() {
+        return tipo;
+    }
+
+    public String getCasilla() {
+        return casilla;
+    }
+
+    public Jugador getJugador() {
+        return jugador;
+    }
+}
--- src/monopoly/Jugador.java
+++ src/monopoly/Jugador.java
@@ -1,94 +1,63 @@
 package monopoly;
 
 import java.util.ArrayList;
-
+import monopoly.Avatar.TipoAvatar;
 
 public class Jugador {
     private String nombre;
-    private char avatar;
-    private byte turno;
+    private Avatar avatar;
+
     private int fortuna;
     private ArrayList<String> propiedades;
     private ArrayList<String> hipotecas;
     private ArrayList<String> edificios;
 
+    /** Crea un Jugador dado su nombre, tipo de avatar e id */
+    public Jugador(String nombre, TipoAvatar tipo, char id) {
+        avatar = new Avatar(tipo, id, this);
+        this.nombre = nombre;
+
+        fortuna = 0;
+        propiedades = new ArrayList<>();
+        hipotecas = new ArrayList<>();
+        edificios = new ArrayList<>();
+    }
+
+    @Override
+    public String toString() {
+        return """
+                {
+                    nombre: %s
+                    avatar: %c
+                    fortuna: %s
+                    propiedades: %s
+                    hipotecas: %s
+                    edificios: %s
+                }
+                """.formatted(nombre, avatar.getId(), fortuna, propiedades, hipotecas, edificios);
+    }
+
     public String getNombre() {
         return nombre;
     }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public char getAvatar() {
+    public Avatar getAvatar() {
         return avatar;
-    }
-
-    public void setAvatar(char avatar) {
-        this.avatar = avatar;
-    }
-
-    public byte getTurno() {
-        return turno;
-    }
-
-    public void setTurno(byte turno) {
-        this.turno = turno;
     }
 
     public int getFortuna() {
         return fortuna;
     }
 
-    public void setFortuna(int fortuna) {
-        this.fortuna = fortuna;
-    }
-
     public ArrayList<String> getPropiedades() {
         return propiedades;
-    }
-
-    public void setPropiedades(ArrayList<String> propiedades) {
-        this.propiedades = propiedades;
     }
 
     public ArrayList<String> getHipotecas() {
         return hipotecas;
     }
 
-    public void setHipotecas(ArrayList<String> hipotecas) {
-        this.hipotecas = hipotecas;
-    }
-
     public ArrayList<String> getEdificios() {
         return edificios;
-    }
-
-    public void setEdificios(ArrayList<String> edificios) {
-        this.edificios = edificios;
-    }
-
-    /*
-    @Override
-    public String toString() {
-        return "{\n" +
-                "nombre: " + nombre + ",\n" +
-                "avatar: " + avatar + "\n" +
-                "}";
-    }
-
-    public void jugador() {
-        System.out.println(toString());
-    }*/
-    public void jugador(){
-        System.out.println("nombre: " + getNombre() + ",");
-        System.out.println("avatar: " + getAvatar() + ",");
-    }
-
-
-    public void crearJugador(String nombre, char avatar){
-        setNombre(nombre);
-        setAvatar(avatar);
-        jugador();
     }
 }
