babea3a22ecec1f88a3e077021e789d41ad18e6d
MarcosG <marcosgranja317@gmail.com>
2023-11-06 13:23:56 +0100

    Mover tests a su carpeta. Crear test de edificar

---
+++ tests/edificar.cmd
@@ -0,0 +1,33 @@
+# Archivo de prueba para comprobar que edificar funciona
+
+# Crear partida
+crear jugador Jugador1 coche
+crear jugador nadie coche
+iniciar
+
+# Comprar Polonia
+mover 1 0
+comprar Varsovia
+end
+
+mover -1 1
+end
+
+mover 2 0
+comprar Lodz
+end
+
+mover -1 1
+end
+
+mover 1 0
+
+# Ver los cambios
+describir Cracovia
+comprar Cracovia
+describir Cracovia
+edificar casa
+describir Cracovia
+
+end
+quit
