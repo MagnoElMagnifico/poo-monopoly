a1bbf4d90e5b0618b9a349906f589815422c77c1
Gabriel <gabrielop2004@gmail.com>
2023-11-13 12:54:55 +0100

    Bugfixes pelota, coche ya se les cobra al retrocer pro la salida

--- src/monopoly/jugadores/Avatar.java
+++ src/monopoly/jugadores/Avatar.java
@@ -116,10 +116,10 @@ public class Avatar {
         int posNuevaCasilla;
         if (movimientoEspecial) {
             posNuevaCasilla = switch (tipo) {
-                case Coche -> moverEspecialCoche(dado, casillas.size());
+                case Coche -> moverEspecialCoche(dado, calculadora, casillas.size());
                 case Esfinge -> moverEspecialEsfinge();
                 case Sombrero -> moverEspecialSombrero();
-                case Pelota -> moverEspecialPelota(dado, casillas.size());
+                case Pelota -> moverEspecialPelota(dado, calculadora,casillas.size());
             };
         } else {
             posNuevaCasilla = moverBasico(dado);
@@ -274,7 +274,7 @@ public class Avatar {
         return true;
     }
 
-    private int moverEspecialCoche(Dado dado, int nCasillas) {
+    private int moverEspecialCoche(Dado dado, Calculadora calculadora, int nCasillas) {
         // Primera tirada
         if (lanzamientos == 0) {
             lanzamientos = 2;
@@ -317,7 +317,14 @@ public class Avatar {
             lanzamientosEspeciales = 0;
             penalizacion = 2;
             lanzamientos = 0;
-            return (this.casilla.getPosicion() - dado.getValor() + nCasillas) % nCasillas;
+            int cantidad = this.casilla.getPosicion() - dado.getValor();
+            if(cantidad<0){
+                if(getJugador().cobrar(calculadora.calcularAbonoSalida())) {
+                    Consola.error("No puedes devolver el abono de la carcel.");
+                }
+                cantidad+=nCasillas;
+            }
+            return cantidad;
         }
         return this.casilla.getPosicion() + dado.getValor();
     }
@@ -332,10 +339,19 @@ public class Avatar {
         return -1;
     }
 
-    private int moverEspecialPelota(Dado dado, int nCasillas) {
+    private int moverEspecialPelota(Dado dado, Calculadora calculadora, int nCasillas) {
         // Comportamieto normal
         if (!pelotaMovimiento) {
-            if (dado.getValor() <= 4) return (this.casilla.getPosicion() - dado.getValor() + nCasillas) % nCasillas;
+            if (dado.getValor() <= 4) {
+                int cantidad = this.casilla.getPosicion() - dado.getValor();
+                if(cantidad<0){
+                    if(getJugador().cobrar(calculadora.calcularAbonoSalida())) {
+                        Consola.error("No puedes devolver el abono de la carcel.");
+                    }
+                    cantidad+=nCasillas;
+                }
+                return cantidad;
+            }
             if (dado.getValor() == 5) return this.casilla.getPosicion() + dado.getValor();
         }
         // Hay que mover más de una vez. 1º vez
